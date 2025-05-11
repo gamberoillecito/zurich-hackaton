566799fb32b9f88010342c5666858f9d97e6c5c9
Darkrucola <pakera008@gmail.com>
2025-05-11 02:38:49 +0200

    testing finale

--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+from scipy.optimize import minimize
+import cvxpy as cp
 
 #import dictionary
 from marc.LSTM import dictWeightedAssets
@@ -11,7 +13,7 @@ with open("marc\selected_assets test1.json") as file:
 dictAssets=dictWeightedAssets(data)
 
 #parameters to tune
-samples=1000 #sample random weights
+samples=1 #sample random weights
 n= 20 #numero asset
 
 #variables definition
@@ -49,5 +51,44 @@ for l in range(samples):
     
 valcas=np.mean(pocoguad)
 
+#test with all equal weights
+equalguad=[]
+meanweight=1/n
+equalguad=np.sum(np.dot(differenza, meanweight))
+
+ritorni = differenza 
+
+# Funzione obiettivo da minimizzare (negativo del guadagno totale)
+def obiettivo(pesi):
+    guadagno_totale = np.sum(pesi * ritorni)
+    return -guadagno_totale  # Minimizzare il negativo del guadagno per massimizzarlo
+
+# Restrizioni: la somma dei pesi deve essere 1 e i pesi devono essere >= 0
+vincolo_somma = {'type': 'eq', 'fun': lambda pesi: np.sum(pesi) - 1}
+vincoli_min = [{'type': 'ineq', 'fun': lambda pesi, i=i: pesi[i] - 0.01} for i in range(n)]
+vincoli_max = [{'type': 'ineq', 'fun': lambda pesi, i=i: 0.40 - pesi[i]} for i in range(n)]
+
+restrizioni = [vincolo_somma] + vincoli_min + vincoli_max
+
+# Condizioni iniziali (distribuzione uguale tra gli asset)
+pesi_iniziali = np.full(n, 1/n)
+
+# Risoluzione del problema di ottimizzazione
+risultato = minimize(obiettivo, pesi_iniziali, constraints=restrizioni)
+
+# Pesi ottimali
+pesi_ottimali = risultato.x
+
+# Calcolare il guadagno totale con i pesi ottimali
+guadagno_totale = np.sum(pesi_ottimali * ritorni)
+
+# Risultati
+print("Pesi ottimali per massimizzare il guadagno:", pesi_ottimali)
+print("Guadagno totale:", guadagno_totale)
+
+
+
+
 print("untrained:",valcas)
+print("equal:", equalguad)
 print("trained:",guadagno)
