2cd291f5972ae3238cd4c3f6144730bb4ea9729c
Marcuji <march@HP-Marcuji>
2025-05-11 04:33:29 +0200

    Pruner in ENG

--- utils/DataPruner.py
+++ utils/DataPruner.py
@@ -5,14 +5,14 @@ from collections import defaultdict
 class DataPruner:
     def __init__(self, file_path):
         pass
-        # Leggi il dataset dal file JSON
+        # Read the dataset from the JSON file
         with open(file_path) as file:
             self.data = json.load(file)
 
     def select_assets(self, num_companies_to_select):
-        '''Given an input datasets, uses different euristics
+        '''Given an input dataset, uses different heuristics
         to select `num_companies_to_select` assets'''
-        # Raggruppiamo le aziende per industria, settore e regione geografica
+        # Group companies by industry, sector, and geographical region
         grouped_by_industry_sector_region = defaultdict(list)
 
         for ticker, details in self.data.items():
@@ -21,21 +21,21 @@ class DataPruner:
             region = details['region']
             grouped_by_industry_sector_region[(industry, sector, region)].append(ticker)
 
-        # Determina il numero di aziende da selezionare
+        # Determine the number of companies to select
         total_groups = len(grouped_by_industry_sector_region)
         companies_per_group = num_companies_to_select // total_groups
 
-        # Seleziona casualmente le aziende da ciascun gruppo
+        # Randomly select companies from each group
         selected_companies = {}
         for group, tickers in grouped_by_industry_sector_region.items():
-            # Seleziona un numero casuale di aziende dal gruppo, rispettando la quantità desiderata
+            # Select a random number of companies from the group, respecting the desired amount
             selected_tickers = random.sample(tickers, min(companies_per_group, len(tickers)))
             selected_companies.update({ticker: self.data[ticker] for ticker in selected_tickers})
 
-        # Se il numero totale di aziende selezionate non raggiunge i 100, aggiungi aziende casuali
+        # If the total number of selected companies is less than 100, add random companies
         while len(selected_companies) < num_companies_to_select:
             remaining = num_companies_to_select - len(selected_companies)
-            # Aggiungi aziende casuali dalla lista rimanente
+            # Add random companies from the remaining list
             remaining_tickers = [ticker for ticker in self.data if ticker not in selected_companies]
             additional_tickers = random.sample(remaining_tickers, remaining)
             selected_companies.update({ticker: self.data[ticker] for ticker in additional_tickers})
