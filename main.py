f78c083f726ba3b67d8592f57ccf3a382237c279
gamberoillecito <itjustfordrive@gmail.com>
2025-05-11 11:22:30 +0200

    corrected other bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import portfolio
 def run(input_data,solver_params,extra_arguments):
     if 'evaluation_date' in extra_arguments:
         input_data['evaluation_date']=extra_arguments['evaluation_date']
-
+    solver = 'qubo'
     if 'ising' in extra_arguments:
         solver = 'ising'
     return portfolio.run(input_data, solver=solver)
