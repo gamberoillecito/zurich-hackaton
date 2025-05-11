c001923ce2c7b76ae9e4a57dcf0e104e3dc69782
gamberoillecito <itjustfordrive@gmail.com>
2025-05-11 08:45:57 +0200

    updated main

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def run(input_data,solver_params,extra_arguments):
         input_data['evaluation_date']=extra_arguments['evaluation_date']
     return portfolio.run(input_data)
 
-if __name__ == "__main__":
-    import json
-    with open("./eth_hackathon/input.json") as file:
-        data = json.load(file)
-    run(data , solver_params=None, extra_arguments={'evaluation_date': '2024-05-12'})
+# if __name__ == "__main__":
+#     import json
+#     with open("./eth_hackathon/input.json") as file:
+#         data = json.load(file)
+#     run(data , solver_params=None, extra_arguments={'evaluation_date': '2024-05-12'})
